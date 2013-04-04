3597c92f346e3551b4dc0a1a0fc7269137cf0974
emris <emris@pc-net.at>
2013-04-05 00:48:00 +0200

    this too

--- src/emris/mods/LeatherWaterSacTFC/LeatherWaterSac.java
+++ src/emris/mods/LeatherWaterSacTFC/LeatherWaterSac.java
@@ -2,28 +2,22 @@ package emris.mods.LeatherWaterSacTFC;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import TFC.TFCItems;
 import TFC.Core.Recipes;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.Mod.PreInit;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid="LeatherWaterSac", name="Leather Water Sac", version="1.2.b75", dependencies = "after:TerraFirmaCraft")
-@NetworkMod(clientSideRequired = true, serverSideRequired = true, versionBounds = "[1.2.b75]")
+@Mod(modid="LeatherWaterSac", name="Leather Water Sac", version="1.3.b76", dependencies = "after:TerraFirmaCraft")
+@NetworkMod(clientSideRequired = true, serverSideRequired = true, versionBounds = "[1.3.b76]")
 public class LeatherWaterSac {
 	@Instance("LeatherWaterSac")
 	public static LeatherWaterSac instance;
-	
-	@SidedProxy(clientSide="emris.mods.LeatherWaterSacTFC.ClientProxy", serverSide="emris.mods.LeatherWaterSacTFC.CommonProxy")
-	public static CommonProxy proxy;
 	
 	public final static Item itemLeatherWaterSac = new ItemLeatherWaterSac(5252);
 
@@ -32,24 +26,16 @@ public class LeatherWaterSac {
 	
 	@Init
 	public void load(FMLInitializationEvent event) {
-		proxy.registerRenderers();
-		
-		LanguageRegistry.addName(itemLeatherWaterSac, "Leather Water Sac");
 		ItemStack lwSac = new ItemStack(this.itemLeatherWaterSac, 1, this.itemLeatherWaterSac.getMaxDamage());
-		ItemStack leather = new ItemStack(Item.leather, 1);
+		ItemStack leather = new ItemStack(TFCItems.PrepHide, 1);
 		ItemStack string = new ItemStack(Item.silk, 1);
 		
 		Item[] tfcKnives = Recipes.Knives;
 		for(int j = 0; j < tfcKnives.length; j++) {
-			GameRegistry.addShapelessRecipe(lwSac, leather, string, new ItemStack(tfcKnives[j], 1, -1));
+			GameRegistry.addShapelessRecipe(lwSac, leather, string, new ItemStack(tfcKnives[j], 1, 32767));
 		}
 		
 		GameRegistry.registerCraftingHandler(new CraftingHandler());
 	}
 	
-	@PostInit
-	public void postInit(FMLPostInitializationEvent event) {
-		// Stub Method
-		// Working with other MODs
-	}
 }
